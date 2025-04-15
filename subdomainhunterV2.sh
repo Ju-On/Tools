b1698cdceb4d442443ecc7724ab8433dd76416e1
vvv <65958004+Ju-On@users.noreply.github.com>
2025-04-16 01:04:14 +1000

    Update subdomainhunterV2.sh

--- subdomainhunterV2.sh
+++ subdomainhunterV2.sh
@@ -1,7 +1,8 @@
 ## subdomain hunting with amass, assetfinder, subfinder into found.txt
 ## httprobe to veriffy alive domains, dedup, strips https:// > alive.txt
 ## gowitness to screenshot > urlofdomain/screenshots
-## http only, replace in httprove section with `| httprobe -prefer-https -p http | grep http |`
+## http only, replace in httprobe section with `| httprobe -prefer-https -p http | grep http |`
+## add # infront of the subdomain tool to exclude scan
 
 #!/bin/bash
 
